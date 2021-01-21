fa38abba2a83119ce61f02e72557604da62ca74a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-01-21 20:38:56 +0100

    Using a fluid interface for configuring API requests

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.Collections.Concurrent;
 using static Lombiq.SetupExtensions.Constants.ConfigurationKeys;
 
@@ -13,38 +12,37 @@ namespace Lombiq.SetupExtensions.Extensions
     public static class ServiceCollectionExtensions
     {
         /// <summary>
-        /// Configures Orchard Core CMS with all API requests as authorized, i.e. all API endpoints will be accessible
+        /// Configures Orchard Core with all API requests as authorized, i.e. all API endpoints will be accessible
         /// without further authorization if the specified configuration value ( <see
-        /// cref="AuthorizeOrchardApiRequests"/>) is set to "true".
+        /// cref="AuthorizeOrchardApiRequests"/>) is set to <see langword="true" />.
         /// </summary>
-        /// <param name="services">Service collection instance from the Startup class' ConfigureServices method.</param>
         /// <param name="configuration">
         /// Application configuration where to read the <see cref="AuthorizeOrchardApiRequests"/> key from.
         /// </param>
-        /// <param name="orchardCoreBuilder">Optional action to define further Orchard CMS configuration.</param>
-        public static IServiceCollection AddOrchardCmsWithAuthorizedApiRequestsIfEnabled(
-            this IServiceCollection services,
-            IConfiguration configuration,
-            Action<OrchardCoreBuilder> orchardCoreBuilder = default) =>
+        public static OrchardCoreBuilder AddAuthorizedApiRequestsIfEnabled(
+            this OrchardCoreBuilder builder,
+            IConfiguration configuration) =>
             configuration.GetValue<bool>(AuthorizeOrchardApiRequests)
-                ? services.AddOrchardCms(builder =>
-                    {
-                        builder
-                            .AddSetupFeatures("OrchardCore.Tenants")
-                            .ConfigureServices(services =>
-                            {
-                                services.AddScoped<IAuthorizationHandler, PermissionContextAuthorizationHandler>(sp =>
-                                    new PermissionContextAuthorizationHandler(
-                                        sp.GetRequiredService<IHttpContextAccessor>(),
-                                        new ConcurrentDictionary<string, PermissionsContext>()));
+                ? builder.AddAuthorizedApiRequests()
+                : builder;
 
-                                services.AddAuthentication(
-                                    options => options.AddScheme<AlwaysLoggedInApiAuthenticationHandler>("Api", null));
-                            })
-                            .Configure(appBuilder => appBuilder.UseAuthorization());
+        /// <summary>
+        /// Configures Orchard Core with all API requests as authorized, i.e. all API endpoints will be accessible
+        /// without further authorization.
+        /// </summary>
+        public static OrchardCoreBuilder AddAuthorizedApiRequests(this OrchardCoreBuilder builder) =>
+            builder
+                .AddSetupFeatures("OrchardCore.Tenants")
+                .ConfigureServices(services =>
+                {
+                    services.AddScoped<IAuthorizationHandler, PermissionContextAuthorizationHandler>(sp =>
+                        new PermissionContextAuthorizationHandler(
+                            sp.GetRequiredService<IHttpContextAccessor>(),
+                            new ConcurrentDictionary<string, PermissionsContext>()));
 
-                        orchardCoreBuilder?.Invoke(builder);
-                    })
-                : services.AddOrchardCms(orchardCoreBuilder);
+                    services.AddAuthentication(
+                        options => options.AddScheme<AlwaysLoggedInApiAuthenticationHandler>("Api", null));
+                })
+                .Configure(appBuilder => appBuilder.UseAuthorization());
     }
 }
